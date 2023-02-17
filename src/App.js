4f8725ec84c9b7d28d77359560b3126060f03bdb
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-02-18 02:00:02 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 import Home from './components/home';
 import Price from './components/priceList';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Price/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route path='canteen' element={<Price/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+      
+   
   );
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,33 @@
 import React from "react";
 import Price from "./priceList";
 import './home.css'
+import { Link } from "react-router-dom";
 
 const Home=()=>{
 return (
-  <div className="body">
-    <div className="container body">
+  <div>
+    <div className="container">
       <div className="row">
         <div className="col-3">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Price List</h5>
+              <h5 className="card-title">Canteen Matters</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
-              </a>
+              <Link to="/canteen">
+              <button type="button" class="btn btn-primary">
+                Price List
+              </button></Link>
+              <Link to="/canteen">
+              <button type="button" class="btn btn-primary">
+                Compliant
+              </button></Link>
+              <Link to="/canteen">
+              <button type="button" class="btn btn-primary">
+                Review
+              </button></Link>
             </div>
           </div>
         </div>
@@ -35,37 +45,10 @@ return (
             </div>
           </div>
         </div>
-        <div className="col-3">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
+     
       </div>
     </div>
-    {/* <Price/> */}
+
   </div>
 );
 }
